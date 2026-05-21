06977b450257ff3c20ddd1bdb6736cee7f5b231f
Edupizzol <edupizzol16@gmail.com>
2026-05-21 18:15:16 +0000

    Add: Adicionando função de busca por cpf e email no banco de dados

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -7,6 +7,7 @@ from app.utils.validators import is_valid_password, is_valid_birth_date
 def register_user(data):
     name = data.get('name')
     email = data.get('email')
+    cpf = data.get('cpf') 
     password = data.get('password')
     birth_date_str = data.get('birth_date')
 
@@ -15,17 +16,20 @@ def register_user(data):
 
     if not is_valid_birth_date(birth_date_str):
         return {"erro": "A data de nascimento é inválida ou o usuário é menor de 18 anos"}, 400
-
-    existing_user = User.query.filter_by(email=email).first()
-    if existing_user:
+    
+    if find_user_by_email(email):
         return {"erro": "Este e-mail já está cadastrado"}, 409
+
+    if find_user_by_cpf(cpf):
+        return {"erro": "Este CPF já está cadastrado"}, 409
 
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
     new_user = User(
         name=name,
         email=email,
-        password=hashed_password,
+        cpf=cpf,
+        password_hash=hashed_password,
         birth_date=birth_date_str
     )
 
@@ -36,3 +40,13 @@ def register_user(data):
     except Exception as e:
         db.session.rollback()
         return {"erro": "Ocorreu um erro interno ao tentar salvar o usuário."}, 500
+    
+def find_user_by_cpf(cpf: str):
+    if not cpf:
+        return None
+    return db.session.query(User).filter(User.cpf==cpf).first()
+
+def find_user_by_email(email: str):
+    if not email:
+        return None
+    return db.session.query(User).filter(User.email == email).first()
