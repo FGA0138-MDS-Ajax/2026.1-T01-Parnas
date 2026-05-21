73b6d8cde10d230877c75028b9c9609407fc42e4
gabriellcardone-06 <gabriell.cardone@gmail.com>
2026-05-21 16:07:28 -0300

    feat: registro de rotas de usuario no app principal e adiciona documentacao

--- backend/app/routes/user_routes.py
+++ backend/app/routes/user_routes.py
@@ -5,6 +5,26 @@ user_bp = Blueprint("user_bp", __name__)
 
 @user_bp.route("/register", methods=["POST"])
 def register():
+    """
+        Rota para cadastro de um novo usuário.
+
+        Endpoint: POST /users/register
+
+        Corpo da Requisição (JSON esperado):
+        {
+            "name": "Nome do Usuário",
+            "email": "usuario@email.com",
+            "cpf": "12345678900",
+            "password": "SenhaForte123!",
+            "birth_date": "2000-05-25" Formato YYYY-MM-DD
+        }
+
+        Possíveis respostas:
+        - 201 (Created): Conta criada com sucesso.
+        - 400 (Bad Request): Erro de validação (senha fraca ou menor de 16 anos).
+        - 409 (Conflict): E-mail ou CPF já cadastrados no banco.
+        - 500 (Server Error): Erro interno ao salvar no banco.
+        """
     data = request.get_json()
 
     answer, status_code = register_user(data)
