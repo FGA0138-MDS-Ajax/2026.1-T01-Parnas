97d1c101bb736a9058c9ad8e7a81b840b153834b
gabriellcardone-06 <gabriell.cardone@gmail.com>
2026-05-21 15:55:37 -0300

    feat: criacao da rota de cadastro de usuario

--- backend/app/routes/user_routes.py
+++ backend/app/routes/user_routes.py
@@ -1,1 +1,11 @@
 from flask import Blueprint, request, jsonify
+from app.services.user_service import register_user
+
+user_bp = Blueprint("user_bp", __name__)
+
+@user_bp.route("/register", methods=["POST"])
+def register():
+    data = request.get_json()
+
+    answer, status_code = register_user(data)
+    return jsonify(answer), status_code
