9590120f9b9f7bdc5f6c4594fd999be6b2b86e4f
Edupizzol <edupizzol16@gmail.com>
2026-05-23 11:33:20 +0000

    chore: finaliza ajustes pos-merge da feature 4

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -15,4 +15,4 @@ class User(db.Model):
     companies = db.relationship('Company', secondary=user_company, back_populates='users')
 
     def __repr__(self):
-        return f'<User {self.name}>'
+        return f'<User {self.name}>'    
