1e35220f158c28fcac1ef82f75a14e0ab1f125af
juliaamandasl <j2004amanda@gmail.com>
2026-05-24 16:19:32 -0300

    feat: adiciona tela de registro

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CadastroEmpresa from './pages/CadastroEmpresa/CadastroEmpresa';
+import Register from './pages/Register/Register';
 
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div style={{ padding: '20px', color: 'white' }}><h1>Home temporária do CREDIFAB 🚀</h1><p>Vá para <a href="/cadastro-empresa">/cadastro-empresa</a> para ver a tela de cadastro de empresa.</p></div>} />
+        <Route path="/" element={<div style={{ padding: '20px', color: 'white' }}><h1>Home temporária do CREDIFAB 🚀</h1><p>Vá para <a href="/cadastro-empresa">/cadastro-empresa</a> para ver a tela de cadastro de empresa.</p><p>Vá para{' '}<a href="/register">/register</a> para ver a tela de registro.</p></div>}/>
         <Route path="/cadastro-empresa" element={<CadastroEmpresa />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
