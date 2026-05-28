96aab09cbf98120a0b01e1021ebacd9d92b9096c
annaaju <annajuliaprimo@gmail.com>
2026-05-28 12:42:50 -0300

    wip: salva estrutura de rotas com layout base antes do merge

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -4,6 +4,8 @@ import Login from './pages/Login/Login';
 import CadastroEmpresa from './pages/CadastroEmpresa/CadastroEmpresa';
 import { EsqueciSenha } from './pages/EsqueciSenha/EsqueciSenha';
 import { RedefinirSenha } from './pages/RedefinirSenha/RedefinirSenha';
+import Dashboard from './pages/Dashboard/Dashboard';
+import LayoutBase from './components/Layout/LayoutBase';
 
 const ProtectedRoute = ({ children }) => {
   const { isAuthenticated } = useAuth();
@@ -14,20 +16,18 @@ export const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* ROTAS PÚBLICAS */}
         <Route path="/" element={<Navigate to="/login" replace />} />
-       
         <Route path="/login" element={<Login />} />
-        <Route path="/esqueci-senha" element={<EsqueciSenha />} />
+        <Route path="/recuperacao-senha" element={<EsqueciSenha />} />
         <Route path="/redefinir-senha" element={<RedefinirSenha />} />
-          
-        <Route 
-          path="/cadastro-empresa" 
-          element={
-            <ProtectedRoute>
-              <CadastroEmpresa />
-            </ProtectedRoute>
-          } 
-        />
+
+        {/* ROTAS PRIVADAS */}
+        <Route element={<ProtectedRoute><LayoutBase /></ProtectedRoute>}>
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/cadastro-empresa" element={<CadastroEmpresa />} />
+          <Route path="/configuracoes" element={<div>[Mock] Tela de Configurações (Sua tarefa US08)</div>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
