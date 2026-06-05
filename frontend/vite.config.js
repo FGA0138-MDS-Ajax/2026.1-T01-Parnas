1b7b86b3720fb3c06b84ebcd71bfc048e1d04214
annaaju <annajuliaprimo@gmail.com>
2026-06-05 17:23:29 -0300

    feat: integra cadastro de empresa com backend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Redireciona todas as rotas que começam com /api
       '/api': {
         target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
       },
-      // Redireciona as rotas de login/logout (/auth/login) se necessário
       '/auth': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/company': {
         target: 'http://127.0.0.1:5000',
         changeOrigin: true,
         secure: false,
