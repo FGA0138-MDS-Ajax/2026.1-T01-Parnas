5bd1db063d67c293f5da20c21f0e90bb746172c4
gabriellcardone-06 <gabriell.cardone@gmail.com>
2026-06-05 22:49:05 -0300

    feat: cria endpoints de calculo e CRUD para simulacao de credito usando DTOs

---
+++ backend/app/routes/simulation_routes.py
@@ -0,0 +1,63 @@
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import jwt_required, get_jwt_identity
+from marshmallow import ValidationError
+from pygments.lexers import data
+
+from app.schemas.simulation_schema import SimulationCalculateDTO, SimulationSaveDTO
+from app.services.simulation_service import (
+    process_simulation,
+    save_simulation,
+    get_simulation,
+    delete_simulation
+)
+
+simulation_bp = Blueprint("simulation_bp", __name__)
+
+calc_schema = SimulationCalculateDTO()
+save_schema = SimulationSaveDTO()
+
+@simulation_bp.route("/calculate", methods=["POST"])
+@jwt_required()
+
+def calculate():
+    try:
+        data = calc_schema.load(request.get_json())
+    except ValidationError as err:
+        return jsonify({"erros_de_validacao": err.messages}), 400
+
+    company_id = request.json.get("company_id")
+
+    answer = process_simulation(data, company_id)
+    return jsonify(answer), 200
+
+@simulation_bp.route("/", methods=["POST"])
+@jwt_required()
+def create():
+    try:
+        data = save_schema.load(request.get_json())
+    except ValidationError as err:
+        return jsonify({"erros_de_validacao": err.messages}), 400
+
+    current_user = get_jwt_identity()
+    answer, status_code = save_simulation(data, current_user)
+    return jsonify(answer), status_code
+
+@simulation_bp.route("/", methods=["GET"])
+@jwt_required()
+def get_all():
+    company_id = request.args.get("company_id")
+
+    if not company_id:
+        return jsonify({"erro": "O parâmetro company_id é obrigatório."}), 400
+
+    answer, status_code = save_simulation(company_id)
+    return jsonify(answer), status_code
+
+@simulation_bp.route("/", methods=["DELETE"])
+@jwt_required()
+def delete(simulation_id):
+    company_id = request.args.get("company_id")
+    if not company_id:
+        return jsonify({"erro": "O parâmetro company_id é obrigatório."}), 400
+    answer, status_code = delete_simulation(simulation_id, company_id)
+    return jsonify(answer), status_code
